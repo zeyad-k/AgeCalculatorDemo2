71715374e80c573f620bd5ce80c161bcaef3bd8e
Zeyad Abdullatif Abdullwahab Korkor <zayad30982025@f-eng.tanta.edu.eg>
2024-02-08 17:30:13 +0200

    calculating age functionality

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
 const submitButton= document.querySelector('.card__button')
 const inputElement= document.querySelectorAll('.card__input')
 
+// const validateDay= (day)=>{
+// if (day && day > 0 && day <= 31) {
+// 	return true
+// }
+// }
+
 
 const calculateAge = (year, month, day)=>{
  	const  today = new Date()
 	const  birthday = new Date(year, month-1, day)
 	let yourAge = today.getFullYear() - birthday.getFullYear() 
-	const monthDiff = today.getMonth -birthday.getMonth
-	if (monthDiff < 0 ) {
-		yourAge = yourAge -1
+	const monthDiff = today.getMonth() - birthday.getMonth()
+ 
+	const dayDiff = today.getDate() - birthday.getDate()
+
+	if (monthDiff < 0 || (monthDiff==0 && dayDiff <= 0)   ) {
+ 		yourAge = yourAge -1
 	}
 	return yourAge
  }
 
 
 const onclickFunction = () =>{
-alert('you clicked me')
+	const dayInput = document.querySelector('#day')
+	const monthInput = document.querySelector('#month')
+	const yearInput = document.querySelector('#year')
+	const resultElement = document.querySelector('.card__resultValue')
+	 resultElement.textContent = calculateAge(yearInput.value, monthInput.value,dayInput.value )
+// alert('you clicked me')
 }
 
 submitButton.addEventListener('click',onclickFunction)
