a64a7f27d60b02a52ee8aa32118bac3479c656d6
Zeyad Abdullatif Abdullwahab Korkor <zayad30982025@f-eng.tanta.edu.eg>
2024-02-09 15:59:40 +0200

    make powerful validation

--- script.js
+++ script.js
@@ -1,11 +1,49 @@
 const submitButton= document.querySelector('.card__button')
 const inputElement= document.querySelectorAll('.card__input')
 
-// const validateDay= (day)=>{
-// if (day && day > 0 && day <= 31) {
-// 	return true
-// }
-// }
+const validateDay= (day)=>{
+if (day && day > 0 && day <= 31) {
+	return true
+}
+}
+const validateMonth= (month)=>{
+if (month && month > 0 && month <= 12) {
+	return true
+}
+}
+const validateYear= (year)=>{
+	const currentYear = new Date().getFullYear()
+if (year && year > 0 && year <= currentYear) {
+	return true
+}
+}
+
+const isDateValid = (dayInput,monthInput,yearInput)=>{
+	let isValid = [false,false,false]
+
+	if (!validateDay(dayInput.value)) {
+		dayInput.classList.add('card__input--error')
+	}else{
+		isValid[0] = true
+		dayInput.classList.remove('card__input--error')
+	}
+
+	if (!validateMonth(monthInput.value)) {
+		monthInput.classList.add('card__input--error')
+	}else{
+		isValid[1] = true
+		monthInput.classList.remove('card__input--error')
+	}
+
+	if (!validateYear(yearInput.value)) {
+		yearInput.classList.add('card__input--error')
+	}else{
+		isValid[2] = true
+		yearInput.classList.remove('card__input--error')
+	}
+
+	return isValid.every((item) => item === true) 
+}
 
 
 const calculateAge = (year, month, day)=>{
@@ -28,6 +66,12 @@ const onclickFunction = () =>{
 	const monthInput = document.querySelector('#month')
 	const yearInput = document.querySelector('#year')
 	const resultElement = document.querySelector('.card__resultValue')
+
+if (!isDateValid(dayInput,monthInput,yearInput)) {
+resultElement.textContent = '--'
+	return
+}
+
 	 resultElement.textContent = calculateAge(yearInput.value, monthInput.value,dayInput.value )
 // alert('you clicked me')
 }
